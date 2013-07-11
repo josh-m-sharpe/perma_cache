e978bfde8996556b53c6d2c9ee22e6923a23630b
Josh Sharpe <josh.m.sharpe@gmail.com>
2013-07-11 14:10:11 -0400

    remove instance methods, add configuration

--- lib/perma_cache.rb
+++ lib/perma_cache.rb
@@ -1,32 +1,26 @@
 require "perma_cache/version"
 
 module PermaCache
+  include ActiveSupport::Configurable
+  config.version = 1
 
   def perma_cache(method_name, options = {})
     class_eval do
       define_method "#{method_name}_key" do
         key = perma_cache_key(self)
-        key << send(options.delete(:obj)).cache_key if options[:obj]
+        key << send(options[:obj]).cache_key if options[:obj]
         key << method_name
         puts key = key.flatten.reject(&:blank?).join('/').downcase
         key
       end
 
-      define_method "#{method_name}_inst_var" do
-        puts inst_var = "@" + send("#{method_name}_key").gsub(/[^0-9a-zA-Z]/,'_')
-        inst_var
-      end
-
       define_method "#{method_name}!" do
         send("#{method_name}_without_perma_cache").tap do |result|
-          instance_variable_set(send("#{method_name}_inst_var"), result)
-          puts "whoamigod writing cache"
           Rails.cache.write(send("#{method_name}_key"), result, :expires_in => 120.hours)
         end
       end
 
       define_method "#{method_name}_with_perma_cache" do
-        instance_variable_get(send("#{method_name}_inst_var")) ||
         Rails.cache.read(send("#{method_name}_key")) ||
         send("#{method_name}!")
       end
@@ -36,7 +30,7 @@ module PermaCache
         def perma_cache_key(obj)
           [
             "perma_cache",
-            # MEMOIZER_VERSION,
+            "v#{PermaCache.config.version}",
             (obj.cache_key rescue nil) || (obj.name rescue nil) || obj.class.name
           ]
         end
