e2c7705e6e751a136ad0b415c0a9dab69aab102f
Josh Sharpe <josh.m.sharpe@gmail.com>
2013-08-13 12:28:25 -0400

    Allow cache to be changed

--- lib/perma_cache.rb
+++ lib/perma_cache.rb
@@ -2,11 +2,19 @@ require "perma_cache/version"
 
 module PermaCache
   def self.version= v
-    @version ||= v
+    @version = v
   end
 
   def self.version
     @version || 1
+  end
+
+  def self.cache
+    @cache ||= Rails.cache
+  end
+
+  def self.cache= c
+    @cache = c
   end
 
   def perma_cache(method_name, options = {})
@@ -25,12 +33,12 @@ module PermaCache
 
       define_method "#{method_name}!" do
         send("#{method_name}_without_perma_cache").tap do |result|
-          Rails.cache.write(send("#{method_name}_key"), result, :expires_in => 72.hours)
+          PermaCache.cache.write(send("#{method_name}_key"), result, :expires_in => 72.hours)
         end
       end
 
       define_method "#{method_name}_with_perma_cache" do
-        Rails.cache.read(send("#{method_name}_key")) ||
+        PermaCache.cache.read(send("#{method_name}_key")) ||
         send("#{method_name}!")
       end
       alias_method_chain method_name, :perma_cache
